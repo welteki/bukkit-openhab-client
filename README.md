b8374359521b2722455445e41082e8c40f0c0bb7
Han Verstraete <verstraete.han@gmail.com>
2018-06-08 07:47:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+# bukkit-openhab-client
+
+NodeJs client for the Minecraft [bukkit-openhab-plugin](https://github.com/ibaton/bukkit-openhab-plugin)
+
+## Installation
+
+```sh
+npm install bukkit-openhab-client
+```
+
+## Example
+
+```javascript
+const OH = require('bukkit-openhab-client')
+
+let client = OH.connect('localhost');
+
+client.on('connect', function () {
+  console.log('connected'); 
+})
+
+client.on('message', function (msg) {
+  console.log(msg);
+})
+
+client.on('player', function (msg) {
+  console.log(msg)
+})
+```
+
+## API
+### OH.connect(host, [options])
+Connect to a server specified by the given url and returns the `Client`
+
+The arguments are:
+* `host`: bukkit server host
+* `options`
+  * `reconnect`: attempt reconnect, **default** = `true`
+  * `reconnectInterval`: the interval to try a reconnect, **default** = `6000`
+  * `maxReconnect`: maximum reconnection attempts
+
+### OH.Client(host, [options])
+Accepts the same arguments as described in the connect function
+
+#### Event `'connect'`
+`function () {}`
+
+Emitted on successful (re)connection
+
+#### Event `'close'`
+`function (code) {}`
+
+Emitted after a disconnection.
+* `code` disconnection status code
+
+#### Event `'reconnect'`
+`function () {}`
+
+Emitted when a reconnect starts.
+
+#### Event `'message'`
+`function (message) {}`
+
+Emitted when the client receives a message
+* `message`
+  * `messageType` number indicating the type of the message
+  * `message` the message content
+
+#### Event `'player'`
+`function (message) {}`
+
+Emitted when the client receives a player message
+
+example player message:
+```javascript
+[
+    {
+      displayName: 'welteki',
+      name: 'welteki',
+      level: 0,
+      totalExperience: 0,
+      experience: 0.0,
+      health: 13.333335876464844,
+      healthScale: 20.0,
+      walkSpeed: 0.2,
+      location: {
+        x: 374.54182933731164,
+        y: 74.0,
+        z: 262.8878343833861,
+        pitch: 18.899847,
+        yaw: 143.69923
+      },
+      gameMode: 'CREATIVE'
+    }
+  ]
+```
+
+#### Event `'sign'`
+`function (message) {}`
+
+Emitted when the client receives a sign message
+
+example sign message:
+```javascript
+[
+    {
+      name: 'Foo',
+      state: true,
+      location: {
+        x: 372.0,
+        y: 75.0,
+        z: 260.0,
+        pitch: 0.0,
+        yaw: 0.0
+      }
+    }
+  ]
+```
+
+#### Event `'server'`
+`function (message) {}`
+
+Emitted when the client recieves a server message
+
+example server message:
+```javascript
+{
+  name: 'CraftBukkit',
+  version: 'git-Spigot-549c1fa-2ee49b4 (MC: 1.12.2)',
+  bukkitVersion: '1.12.2-R0.1-SNAPSHOT',
+  maxPlayers: 20,
+  players: 1
+}
+```
+
+### OH.Client#close()
+Close websocket connection to the server
---
+++ index.ts
@@ -0,0 +1,154 @@
+import * as Websocket from 'ws';
+import { EventEmitter } from 'events';
+import { URL } from 'url';
+
+enum message {
+  PLAYER_MESSAGE            = 1,
+  SERVER_MESSAGE            = 2,
+  PLAYER_COMMANDS_MESSAGE   = 3,
+  SIGN_MESSAGE              = 4,
+  SIGN_COMMANDS_MESSAGE     = 5
+}
+
+enum websocketState {
+  CONNECTING,
+  OPEN,
+  CLOSING,
+  CLOSED
+}
+
+enum websocketCloseStatus {
+  WEB_SOCKET_SUCCESS_CLOSE_STATUS                 = 1000,
+  WEB_SOCKET_ENDPOINT_UNAVAILABLE_CLOSE_STATUS    = 1001,
+  WEB_SOCKET_PROTOCOL_ERROR_CLOSE_STATUS          = 1002,
+  WEB_SOCKET_INVALID_DATA_TYPE_CLOSE_STATUS       = 1003,
+  WEB_SOCKET_EMPTY_CLOSE_STATUS                   = 1005,
+  WEB_SOCKET_ABORTED_CLOSE_STATUS                 = 1006,
+  WEB_SOCKET_INVALID_PAYLOAD_CLOSE_STATUS         = 1007,
+  WEB_SOCKET_POLICY_VIOLATION_CLOSE_STATUS        = 1008,
+  WEB_SOCKET_MESSAGE_TOO_BIG_CLOSE_STATUS         = 1009,
+  WEB_SOCKET_UNSUPPORTED_EXTENSIONS_CLOSE_STATUS  = 1010,
+  WEB_SOCKET_SERVER_ERROR_CLOSE_STATUS            = 1011,
+  WEB_SOCKET_SECURE_HANDSHAKE_ERROR_CLOSE_STATUS  = 1015
+}
+
+export interface ClientOptions {
+  port?: number;
+  reconnect?: boolean;
+  reconnectInterval?: number;
+  maxReconnects?: number;
+}
+
+const DEFAULT_PORT: number = 10692;
+const DEFAULT_PATH: string = 'stream';
+
+export class Client extends EventEmitter {
+  private options: ClientOptions
+  private url: URL;
+  private ws: Websocket;
+  private reconnectCount: number = 0;
+
+  constructor(host: string, options?: ClientOptions) {
+    super();
+    options = options || {};
+    options.port = options.port || DEFAULT_PORT;
+    options.reconnect = options.reconnect !== false;
+    options.reconnectInterval = options.reconnectInterval || 6000;
+    this.options = options;
+
+    let wsURL: URL;
+    try {
+      wsURL = new URL(DEFAULT_PATH, host);
+    } catch {
+      wsURL = new URL(DEFAULT_PATH, `ws://${host}`)
+    }
+    wsURL.protocol = 'ws';
+    //@ts-ignore
+    wsURL.port = wsURL.port ? wsURL.port : this.options.port.toString()
+    this.url = wsURL;
+    console.log(wsURL.href);
+
+    this.connect();
+  }
+
+  private connect() {
+    let ws = new Websocket(this.url.href);
+    this.ws = ws;
+
+    ws.on('error', (err) => {
+      return;
+    })
+
+    ws.on('open', () => {
+      this.emit('connect');
+    })
+
+    ws.on('close', (code, reason) => {
+      this.emit('close', code);
+      if (code === websocketCloseStatus.WEB_SOCKET_SUCCESS_CLOSE_STATUS) return;
+      if (this.options.reconnect) this.reconnect(this.options.reconnectInterval);
+    })
+
+    ws.on('message', (msg) => {
+      this.onMessage(JSON.parse(msg.toString()));
+    });
+  }
+
+  close(): void {
+    this.ws.close(websocketCloseStatus.WEB_SOCKET_SUCCESS_CLOSE_STATUS);
+  }
+
+  private reconnect(timeout?: number): void {
+    if (this.options.maxReconnects === this.reconnectCount) {
+      this.close();
+      return;
+    }
+
+    const reconnect = () => {
+      this.emit('reconnect');
+      this.connect();
+      this.reconnectCount++
+    }
+
+    if (timeout) {
+      setTimeout(reconnect, timeout);
+    } else {
+      reconnect();
+    }
+  }
+
+  private onMessage(data: any): void {
+    switch (data.messageType) {
+      case message.PLAYER_MESSAGE:
+        this.onPlayerMessage(data.message);
+        break;
+      case message.SERVER_MESSAGE:
+        this.onServerMessage(data.message);
+        break;
+      case message.SIGN_MESSAGE:
+        this.onSignMessage(data.message);
+        break;
+      default:
+        console.log(`unknown message type: ${data}`);
+    }
+
+    this.emit('message', data);
+  }
+
+  private onPlayerMessage(message): void {
+    this.emit('player', message);
+  }
+
+  private onServerMessage(message): void {
+    this.emit('server', message);
+  }
+
+  private onSignMessage(message): void {
+    this.emit('sign', message);
+  }
+}
+
+export function connect (host: string, options?: ClientOptions): Client {
+  const client = new Client(host, options);
+  return client;
+}
